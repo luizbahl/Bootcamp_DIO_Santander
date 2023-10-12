d2e5cd5c21b265dd4a4c7b8c19e1c9c609be1c2d
LuizBahl <luizbahl@hotmail.com>
2023-10-12 20:32:30 -0300

    Conhecendo o Drawer

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          primarySwatch: Colors.red,
+          primarySwatch: Colors.blueGrey,
           textTheme: GoogleFonts.mansalvaTextTheme()),
       home: const LoginPage(),
     );
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -10,8 +10,49 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Meu App")),
-    );
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(title: const Text("Main Page")),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              InkWell(
+                child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    width: double.infinity,
+                    child: const Text("Dados cadastrais")),
+                onTap: () {},
+              ),
+              const Divider(),
+              const SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    width: double.infinity,
+                    child: const Text("Configurações")),
+                onTap: () {},
+              ),
+              const Divider(),
+              const SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    width: double.infinity,
+                    child: const Text("Termos de uso e Privacidade")),
+                onTap: () {},
+              ),
+              const Divider()
+            ],
+          ),
+        ),
+      ),
+    ));
   }
 }
