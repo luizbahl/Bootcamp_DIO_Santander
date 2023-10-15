a33320bb0c32b4ffd5d7d5504d660a7f8407f735
LuizBahl <luizbahl@hotmail.com>
2023-10-15 20:24:37 -0300

    Usando o Hero

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/model/card_detail.dart';
+import 'package:trilhaapp/pages/card_detail.dart';
 
 class CardPage extends StatefulWidget {
   const CardPage({super.key});
@@ -8,6 +10,12 @@ class CardPage extends StatefulWidget {
 }
 
 class _CardPageState extends State<CardPage> {
+  var cardDetail = CardDetail(
+      1,
+      "Meu Card",
+      "https://hermes.digitalinnovation.one/assets/diome/logo.png",
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi arquiteto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam. , quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex e a comodi consequatur? ");
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,49 +23,63 @@ class _CardPageState extends State<CardPage> {
         Container(
           margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           width: double.infinity,
-          child: Card(
-            elevation: 8,
-            shadowColor: Colors.grey,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+          child: InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => CardDetailPage(
+                            cardDetail: cardDetail,
+                          )));
+            },
+            child: Hero(
+              tag: cardDetail.id,
+              child: Card(
+                elevation: 8,
+                shadowColor: Colors.grey,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Image.network(
-                        "https://hermes.digitalinnovation.one/assets/diome/logo.png",
-                        height: 20,
+                      Row(
+                        children: [
+                          Image.network(
+                            cardDetail.url,
+                            height: 20,
+                          ),
+                          Text(
+                            cardDetail.title,
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w600),
+                          ),
+                        ],
                       ),
-                      const Text(
-                        "Meu Card",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w600),
+                      const SizedBox(
+                        height: 10,
                       ),
+                      Text(
+                        cardDetail.text,
+                        textAlign: TextAlign.justify,
+                        style: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.normal),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "Ler Mais",
+                              style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  color: Color.fromARGB(255, 33, 143, 36)),
+                            )),
+                      )
                     ],
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi arquiteto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam. , quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex e a comodi consequatur? ",
-                    textAlign: TextAlign.justify,
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
-                  ),
-                  Container(
-                    width: double.infinity,
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          "Ler Mais",
-                          style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              color: Color.fromARGB(255, 33, 143, 36)),
-                        )),
-                  )
-                ],
+                ),
               ),
             ),
           ),
